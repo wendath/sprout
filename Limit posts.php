476f0ea14cc6c850c9b2ce652d416441b15c04ef
wendath <61119855+wendath@users.noreply.github.com>
2020-04-06 00:38:50 +0200

    Create Limit posts.php

---
+++ Limit posts.php
@@ -0,0 +1,26 @@
+<?php
+
+//Hide the Publish Actions on 'post-new.php?post_type=property', pages, but not on edit pages
+
+add_action('admin_head' , 'limit_posts');
+function limit_posts(){
+	$count_property = wp_count_posts('property')->publish;
+	$max_properties = 10; //the number of max published posts
+	if($count_property >= $max_properties){
+		global $pagenow;
+		if (! empty($pagenow) && ('post-new.php' === $pagenow && ( $_GET['post_type'] == 'property'))){
+			?>
+			<style>
+				#publishing-action { display: none; }
+			</style>
+			<script type="text/javascript">
+			jQuery(document).ready(function(){
+				jQuery("#major-publishing-actions").html("You have reached the publication limit for this CPT."); // message is displayed instead of the publish button
+			});
+			</script>
+				<?php
+		}
+	}
+}
+
+?>
